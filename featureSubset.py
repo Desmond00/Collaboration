8bf3f98f8a53f17aee8ba65feb2310ab275820c6
Desmond00 <ranjandebnath.rd@gmail.com>
2018-02-28 19:33:28 +0530

    only unique subsets of featueres are taken

--- featureSubset.py
+++ featureSubset.py
@@ -1,20 +1,38 @@
 import random
 import math
+
+def isRepeat(subsets, tempSet):
+    #print(subsets)
+    #print(tempSet)
+    for subset in subsets:
+        valueSet = set()
+        for feature in subset:
+            valueSet.add(feature)
+        for feature in tempSet:
+            if((feature in valueSet) == False):
+                return False
+
+    
 a = 1
-x = [1,2,5]
-current = 0
+
+subsets = []
+subsets.append(random.sample(range(1,len(xScaled_df.columns)),3))
 r1 = a
 s = int(round(random.random(),1)*10)
-for i in range(5):
-    print('[', end='')
+
+for i in range(4):
+    subset = []
     for j in range(3):
-        r2 = random.randint(1,100)
-        r3 = random.randint(1,100)
-        if(s%2 == 0)
-            current = x[j] + r1*math.sin(r2)*abs(r3*x[j]-x[j])
+        r2 = random.uniform(0.0,2.0)
+        r3 = random.uniform(0.0,2.0)
+        if(s%2 == 0):
+            feature = int(x[j] + r1*math.sin(r2)*abs(r3*x[j]-x[j]))
         else:
-            current = x[j] + r1*math.cos(r2)*abs(r3*x[j]-x[j])
-        r1 = a - t*(a/n) 
-        print(current, sep=',', end='')
-    print(']')
-            
+            feature = int(x[j] + r1*math.cos(r2)*abs(r3*x[j]-x[j]))
+        r1 = a - j*(a/3)
+        subset.append(feature)
+    if(isRepeat(subsets, subset) == False):
+        subsets.append(subset)
+    
+print(subsets)
+#This is the set of subsets without repeatation of features
