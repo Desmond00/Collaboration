602e6e9495a907874c08a951ffaae93070771404
Desmond00 <ranjandebnath.rd@gmail.com>
2018-02-28 17:23:07 +0530

    Initial code for selectiion of random feature subsets using sinCosineAlgorithm, problem of repeatative feature subsets is yet to be removed

---
+++ featureSubset.py
@@ -0,0 +1,20 @@
+import random
+import math
+a = 1
+x = [1,2,5]
+current = 0
+r1 = a
+s = int(round(random.random(),1)*10)
+for i in range(5):
+    print('[', end='')
+    for j in range(3):
+        r2 = random.randint(1,100)
+        r3 = random.randint(1,100)
+        if(s%2 == 0)
+            current = x[j] + r1*math.sin(r2)*abs(r3*x[j]-x[j])
+        else:
+            current = x[j] + r1*math.cos(r2)*abs(r3*x[j]-x[j])
+        r1 = a - t*(a/n) 
+        print(current, sep=',', end='')
+    print(']')
+            
