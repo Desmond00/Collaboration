8ceed1c3e4acec1b7f9ba36b3e6d500bf205e006
Ranjan Debnath <ranjandebnath.rd@gmail.com>
2018-03-01 21:20:20 +0530

    updated r2,r3,s in proper iteration and feature subset loss due to duplicate entries has been fixed

---
+++ featureSubsetRedefined.py
@@ -0,0 +1,44 @@
+import random
+import math
+
+def isRepeat(subsets, tempSet):
+    print(subsets)
+    print(tempSet)
+    for subset in subsets:
+        valueSet = set()
+        for feature in subset:
+            valueSet.add(feature)
+    for feature in tempSet:
+        if((feature in valueSet) == False):
+            return False
+
+    
+a = 1
+Tmax = 4
+subsets = []
+X = random.sample(range(1,len(xScaled_df.columns)),3)
+P = X
+subsets.append(X)
+r1 = a
+
+i = 0
+while i<Tmax:
+    subset = []
+    s = int(round(random.random(),1)*10)
+    r2 = random.uniform(0.0,2.0)
+    r3 = random.uniform(0.0,2.0)
+    r1 = a - i*(a/Tmax)
+    for j in range(3):
+        print(X[j])
+        if(s%2 == 0):
+            feature = int(X[j] + r1*math.sin(r2)*abs(r3*P[j]-X[j]))
+        else:
+            feature = int(X[j] + r1*math.cos(r2)*abs(r3*P[j]-X[j]))
+        subset.append(feature)
+    if(isRepeat(subsets, subset) == False):
+        X = subset
+        subsets.append(subset)
+        i += 1
+    
+print(subsets)
+#This is the set of subsets without repeatation of features
