444c7db8948c88c28882670d1f456a4f4a69b003
Desmond00 <ranjandebnath.rd@gmail.com>
2018-03-05 21:59:03 +0530

    with changed range of r2 and r3 for better feature exploration

--- featureSubset.py
+++ featureSubset.py
@@ -16,19 +16,21 @@ def isRepeat(subsets, tempSet):
 a = 1
 
 subsets = []
+totalFeatures = len(xScaled_df.columns)
 subsets.append(random.sample(range(1,len(xScaled_df.columns)),3))
+p = x[0]
 r1 = a
 s = int(round(random.random(),1)*10)
 
 for i in range(4):
     subset = []
     for j in range(3):
-        r2 = random.uniform(0.0,2.0)
-        r3 = random.uniform(0.0,2.0)
+        r2 = random.uniform(0.0, totalFeatures)
+        r3 = random.uniform(0.1,2.0)
         if(s%2 == 0):
-            feature = int(x[j] + r1*math.sin(r2)*abs(r3*x[j]-x[j]))
+            feature = int(x[j] + r1*r2*abs(r3*p-x[j]))
         else:
-            feature = int(x[j] + r1*math.cos(r2)*abs(r3*x[j]-x[j]))
+            feature = int(x[j] + r1*r2*abs(r3*p-x[j]))
         r1 = a - j*(a/3)
         subset.append(feature)
     if(isRepeat(subsets, subset) == False):
--- featureSubsetRedefined.py
+++ featureSubsetRedefined.py
@@ -1,3 +1,4 @@
+'''
 import random
 import math
 
@@ -34,6 +35,56 @@ while i<Tmax:
             feature = int(X[j] + r1*math.sin(r2)*abs(r3*P[j]-X[j]))
         else:
             feature = int(X[j] + r1*math.cos(r2)*abs(r3*P[j]-X[j]))
+        subset.append(feature)
+    if(isRepeat(subsets, subset) == False):
+        X = subset
+        subsets.append(subset)
+        i += 1
+    
+print(subsets)
+#This is the set of subsets without repeatation of features
+'''
+
+import random
+import math
+
+def isRepeat(subsets, tempSet):
+    print(subsets)
+    print(tempSet)
+    for subset in subsets:
+        valueSet = set()
+        for feature in subset:
+            valueSet.add(feature)
+    for feature in tempSet:
+        if((feature in valueSet) == False):
+            return False
+
+#MODIFIED r2, r3 ranges. It does the exploration part pretty good. Now we have to develop the whole algorithm including model training and prediction score evaluation.
+
+a = 1
+Tmax = 4
+subsets = []
+totalFeatures = len(xScaled_df.columns)
+X = random.sample(range(1,totalFeatures),3)
+P = X
+subsets.append(X)
+r1 = a
+
+i = 0
+while i<Tmax:
+    subset = []
+    s = int(round(random.random(),1)*10)
+    r2 = random.uniform(0.1, 1.0)
+    r3 = random.uniform(0.1,2.0)
+    r1 = a - i*(a/Tmax)
+    for j in range(3):
+        print(X[j])
+        if(s%2 == 0):
+            feature = int(X[j] + r1*r2*abs(r3*P[j]-X[j]))
+        else:
+            feature = int(X[j] + r1*r2*abs(r3*P[j]-X[j]))
+        feature %= totalFeatures
+        feature += 1
         subset.append(feature)
     if(isRepeat(subsets, subset) == False):
         X = subset
